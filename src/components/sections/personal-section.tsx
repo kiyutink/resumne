cb1d732b153432d8ac56a2a4a3f72fed17595840
kiyutink <kiyutin@gmail.com>
2020-11-24 21:50:20 +0100

    Add email as link

--- src/components/sections/personal-section.tsx
+++ src/components/sections/personal-section.tsx
@@ -1,6 +1,47 @@
 import "./personal-section.scss";
-import React from "react";
+import React, { Fragment, useState } from "react";
+import classNames from "classnames";
+import AutosizeInput from "react-input-autosize";
+
 import { Input } from "../shared/input";
+import { useField } from "formik";
+
+export const Email: React.FC<{ name: string; className: string }> = ({
+  name,
+  className,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const toggle = () => setIsEditing((val) => !val);
+  const [field] = useField(name);
+
+  if (isEditing) {
+    return (
+      <span className="email-input">
+        <AutosizeInput
+          {...field}
+          inputClassName={classNames("input", "email-input__input", className)}
+        />
+        <button onClick={toggle} className="email-input__button">
+          <i className="fa fa-check" />
+        </button>
+      </span>
+    );
+  } else {
+    return (
+      <span className="email-input">
+        <a href={`mailto:${field.value}`} className="email-input__link">
+          {field.value}
+        </a>
+        <button
+          onClick={toggle}
+          className="email-input__button email-input__button--edit"
+        >
+          <i className="far fa-edit" />
+        </button>
+      </span>
+    );
+  }
+};
 
 export const PersonalSection: React.FC = () => {
   return (
@@ -14,7 +55,7 @@ export const PersonalSection: React.FC = () => {
       <div className="personal-section__row">
         <Input name="phone" className="personal-section__phone-and-email" />
         <b className="personal-section__separator">·</b>
-        <Input name="email" className="personal-section__email" />
+        <Email name="email" className="personal-section__email" />
       </div>
     </div>
   );
